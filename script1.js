2c6e67d50c42bb5c1f9e08a986b38c17b85fb456
nov21qa <86826333+nov21qa@users.noreply.github.com>
2021-09-07 03:17:09 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const http = require('http');
-const express =require('express');
-const app = express();
-
-app.get("/",(request,response)=>
+fetch("https://clist.by/api/v2/contest/?username=manavesh&api_key=0bc94771bb4c92eefa1ec425f417af50486d6075",
 {
-    response.status(200).end("welcome");
-})
-
-
-app.listen(2000,()=>
-{
-    console.log("server is listening!");
-})
+    method:"GET",
+    origin:"strict-origin-when-cross-origin",
+    
+    headers: {
+        'Content-Type': 'application/json', // Your headers
+        
+        'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Authorization': 'Bearer key',
+        
+      }
+    
+}).then((response)=>
+response.json()).then((data)=>
+data);
